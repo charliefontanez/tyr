3cf60ab2785d41e293c0f42a84f2b592b6d8c728
Carlos Fontanez <charliefontanezis@gmail.com>
2022-04-09 23:55:49 -0400

    Changed movie api, added search list results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,112 @@
-var searchEl = document.querySelector("form")
-    var searchInput;
+var searchEl = document.querySelector("form");
+var searchResults = document.querySelector(".search-results");
+var searchInput = "";
 
-    var getGameData = function(game) {
-      var apiUrl = "https://api.rawg.io/api/games?search=" + game + "&key=1162dfcd4c004cc3adeeaa606112ab38";
-      fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-          response.json().then(function(response) {
-            for (i = 0; i < response.results.length; i++) {
-              console.log(response.results[i].name);
-            }
-            console.log(response);
-          });
-        }
-        else {
-          console.log("Bad response found");
-        }
+var data;
+
+
+var getGameData = function(game) {
+  var apiUrl = "https://api.rawg.io/api/games?search=" + game + "&key=1162dfcd4c004cc3adeeaa606112ab38";
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(response) {
+        console.log(response);
+        showResults(response);
       });
     }
-
-
-
-
-
-
-    $("#apiSwitch").on("click", function() {
-        var gameSearchBar = document.getElementById("#")
-        if (searchEl.id === "game-search-form") {
-            $(searchEl).attr("id", "movie-search-form")
-            $("#search").attr("placeholder", "Search Movies and Tv Shows")
-            console.log(searchEl)
-        }else if (searchEl.id === "movie-search-form") {
-            $(searchEl).attr("id", "game-search-form")
-            $("#search").attr("placeholder", "Search Games")
-            console.log(searchEl)
-        }
-    })
-
-  searchEl.addEventListener("submit", function(e) {
-    e.preventDefault();
-    searchInput = searchEl.children[0].value;
-    
-      if (searchEl.id === "game-search-form") {
-          getGameData(searchInput);
-          $("#search").val("");
-      } else if (searchEl.id === "movie-search-form") {
-          getMovieData(searchInput);
-          searchEl.children[0].value = "";
-      }
-  });
-
-
-  var getMovieData = function(movie) {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'imdb-data-searching.p.rapidapi.com',
-      'X-RapidAPI-Key': 'b3f2425f53msh6ddaa98247cb618p193474jsn1b3a90d328d2'
+    else {
+      console.log("Bad response found");
     }
-  };
+  });
+}
 
-//   var searchInput = searchEl.children[0].value;
+var getMovieData = function(movie) {
+  movie = movie.trim();
 
-
-  fetch('https://imdb-data-searching.p.rapidapi.com/om?s=' + movie, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+  fetch("https://api.themoviedb.org/3/search/movie?api_key=ad632fafb63b4f5e153703fe70e7b5e4&query=" + movie)
+  .then(function(response) {
+    if (response.ok) {
+    response.json().then(function(response) {
+      console.log(response);
+      showResults(response);
+    });
+  }
+  else {
+    console.log("Bad response found");
+  }
+  });
 };
+
+var showResults = function(response) {
+  
+  // document.querySelectorAll(".result-item");
+  
+  removeAllChildren(searchResults);
+
+  if (searchEl.id == "game-search-form") {
+    for (i = 0; i < response.results.length; i++) {
+      var searchItem = document.createElement("div");
+      searchItem.addEventListener("click", itemClickHandler);
+      searchItem.id = "result-" + i;
+      searchItem.className = "result-item";
+      searchItem.textContent = response.results[i].name;
+      searchResults.appendChild(searchItem);
+    }
+  }
+  else {
+    for (i = 0; i < response.results.length; i++) {
+      var searchItem = document.createElement("div");
+      searchItem.addEventListener("click", itemClickHandler);
+      searchItem.id = "result-" + i;
+      searchItem.className = "result-item";
+      searchItem.textContent = response.results[i].title;
+      searchResults.appendChild(searchItem);
+    }
+  }
+}
+
+
+var itemClickHandler = function () {
+  removeAllChildren(searchResults);
+  console.log("Item Clicked");
+  document.querySelector("#item-description").classList.remove("hide");
+  document.querySelector("#item-description").classList.add("show");
+}
+
+
+var removeAllChildren = function(container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+
+// Search box event listener
+searchEl.addEventListener("submit", function(e) {
+  e.preventDefault();
+  searchInput = document.querySelector("#search").value.trim();
+  
+  if (searchEl.id == "game-search-form") {
+    getGameData(searchInput);
+    $("#search").val("");
+  }
+  else if (searchEl.id == "movie-search-form") {
+    getMovieData(searchInput);
+    searchEl.children[0].value = "";
+  }
+
+});
+
+// Button to switch results from movie to games
+$("#apiSwitch").on("click", function() {
+  if (searchEl.id == "game-search-form") {
+    $(searchEl).attr("id", "movie-search-form");
+    $("#search").attr("placeholder", "Search Movies and Tv Shows");
+    console.log(searchEl);
+  }
+  else if (searchEl.id == "movie-search-form") {
+    $(searchEl).attr("id", "game-search-form");
+    $("#search").attr("placeholder", "Search Games");
+    console.log(searchEl);
+  }
+})
